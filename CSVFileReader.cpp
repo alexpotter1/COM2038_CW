fed3571bb2b8ae86e3b045027f414938b303e106
Alex Potter <alexpotter1@icloud.com>
2017-12-12 13:04:05 +0000

    CSVFileReader: adjust sizeof to return number of values in files array

--- CSVFileReader.cpp
+++ CSVFileReader.cpp
@@ -6,7 +6,7 @@ using namespace std;
 CSVFileReader::CSVFileReader() {}
 
 void CSVFileReader::instantiateAnimals(ifstream** files){
-	for(int i = 0; i < sizeof(files); i++){
+	for(int i = 0; i <= (sizeof(files)/sizeof(ifstream)); i++){
 		cout << "Hello" << endl;
 	}
 }
