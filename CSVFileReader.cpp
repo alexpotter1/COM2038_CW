8adc8eeca5c1f233b8c86bf005f64e35f2895358
Alex Potter <alexpotter1@icloud.com>
2017-12-12 12:22:53 +0000

    Attempt to fix ifstream errors

--- CSVFileReader.cpp
+++ CSVFileReader.cpp
@@ -3,7 +3,7 @@
 
 CSVFileReader::CSVFileReader();
 
-void CSVFileReader::instantiateAnimals(ifstream files[]){
+void CSVFileReader::instantiateAnimals(ifstream (*files)[5]){
 	for(int i = 0; i < sizeof(files); i++){
 		cout << "Hello" << endl;
 	}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "CSVFileReader.h"
 
 int main(){
 	ifstream dogsFile("Dogs.csv");
-	ifstream files[1];
-	files[0] = dogsFile;
-	CSVFileReader csvFileReader();
-	csvFileReader.instantiateAnimals(dogsFile);
+	ifstream* files[5];
+	files[0] = &dogsFile;
+	CSVFileReader csvFileReader;
+	csvFileReader.instantiateAnimals(files);
 }
