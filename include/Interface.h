2045d586e34d08d0580ba8a5985a52d9263b14ab
ob00218 <oliver.brahma.pearl@gmail.com>
2017-12-14 17:24:33 +0000

    Implemented search() method

--- include/Interface.h
+++ include/Interface.h
@@ -8,6 +8,9 @@ public:
 	Interface();
 	string printAll();
 	string search();
+	string ltrim(string s, const char* t);
+	string rtrim(string s, const char* t);
+	string trim(string s, const char* t);
 private:
 	StorageManager storageManager;
 };
