4c6686b5c9363e5151e6ea0ea1545b0c913e932b
Ignas <ikampas@yahoo.co.uk>
2017-12-15 09:18:03 -0800

    made 'transformToBase' function be a template function in CSVFileReader.h

--- include/CSVFileReader.h
+++ include/CSVFileReader.h
@@ -3,15 +3,36 @@
 #include "StorageManager.h"
 using namespace std;
 
+//template<class Derived, class Base>
 class CSVFileReader {
 public:
 	CSVFileReader();
 	vector<Dog*>* getDogs(ifstream* files);
 	vector<Cat*>* getCats(ifstream* files);
 	vector<Horse*>* getHorses(ifstream* files);
+	template<class Derived, class Base>
+	vector<Base*> transformToBase(vector<Derived*> &derivedVec);
+	
+
 private:
 	Dog* getDog(string* linePtr);
 	Cat* getCat(string* linePtr);
 	Horse* getHorse(string* linePtr);
 	vector<string>* getAttributes(string* linePtr);
 };
+
+template<class Derived, class Base>
+vector<Base*> CSVFileReader::transformToBase(vector<Derived*> &derivedVec) {
+  class vector<Base*> baseVec;
+  typename vector<Derived*>::iterator iter;
+  Base* base;
+
+  for (iter = derivedVec.begin(); iter != derivedVec.end(); ++iter) {
+    base = dynamic_cast<Base*>(*iter);
+    if (base) {
+      baseVec.push_back(base);
+    }
+  }
+
+  return baseVec;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,6 @@
 #include <vector>
 using namespace std;
 // Dog, Animal: Derived, Base
-template<class Derived, class Base>
-vector<Base*> transformToBase(vector<Derived*> &derivedVec) {
-  class vector<Base*> baseVec;
-  typename vector<Derived*>::iterator iter;
-  Base* base;
-
-  for (iter = derivedVec.begin(); iter != derivedVec.end(); ++iter) {
-    base = dynamic_cast<Base*>(*iter);
-    if (base) {
-      baseVec.push_back(base);
-    }
-  }
-
-  return baseVec;
-}
 
 int main() {
 	ifstream dogsFile("csv/Dogs.csv");
@@ -43,9 +28,9 @@ int main() {
 		cout << horsePtr->getName() << endl;
 	}
 
-  vector<Animal*> animalVects1 = transformToBase<Dog, Animal>(*dogVectsPtr);
-  vector<Animal*> animalVects2 = transformToBase<Cat, Animal>(*catVectsPtr);
-  vector<Animal*> animalVects3 = transformToBase<Horse, Animal>(*horseVectsPtr);
+  vector<Animal*> animalVects1 = csvFileReader.transformToBase<Dog, Animal>(*dogVectsPtr);
+  vector<Animal*> animalVects2 = csvFileReader.transformToBase<Cat, Animal>(*catVectsPtr);
+  vector<Animal*> animalVects3 = csvFileReader.transformToBase<Horse, Animal>(*horseVectsPtr);
 
   animalVects1.insert(animalVects1.end(), animalVects2.begin(), animalVects2.end());
   animalVects1.insert(animalVects1.end(), animalVects3.begin(), animalVects3.end());
