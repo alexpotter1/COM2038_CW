b89fccdec5ee50442cc59b25de2d329aad4d3fb2
Ignas <ikampas@yahoo.co.uk>
2017-12-16 04:05:43 -0800

    CSVFileReader.h and CSVFileReader.cpp comments

--- include/CSVFileReader.h
+++ include/CSVFileReader.h
@@ -3,24 +3,55 @@
 #include "StorageManager.h"
 using namespace std;
 
-//template<class Derived, class Base>
+/*Constructor for the class which is used to read files to store the information into objects. 
+The objects are then saved in memory. The class provides methods to retrieve vectors of 
+pointers to these objects.*/
 class CSVFileReader {
 public:
+	/*Constructor for the CSVFileReader class.*/
 	CSVFileReader();
+	/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+	vector which stores pointers to Dog objects. The Dog objects have been created using attributes 
+	which have been read from the file. The function returns the pointer to the vector of pointers to 
+	Dog objects.*/
 	vector<Dog*>* getDogs(ifstream* files);
+	/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+	vector which stores pointers to Cat objects. The Cat objects have been created using attributes 
+	which have been read from the file. The function returns the pointer to the vector of pointers to 
+	Cat objects.*/
 	vector<Cat*>* getCats(ifstream* files);
+	/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+	vector which stores pointers to Horse objects. The Horse objects have been created using attributes 
+	which have been read from the file. The function returns the pointer to the vector of pointers to 
+	Horse objects.*/
 	vector<Horse*>* getHorses(ifstream* files);
+	/*Template function which is used to dynamically cast an instance of a class derived from the 
+	Animal class to an intance of the Animal class.*/
 	template<class Derived, class Base>
 	vector<Base*> transformToBase(vector<Derived*> &derivedVec);
 	
 
 private:
+	/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+	tokenise the sting into attributes. Then the function populates the Dog object attributes 
+	using the attributes from the string. Then the function returns the pointer to the Dog
+	object.*/
 	Dog* getDog(string* linePtr);
+	/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+	tokenise the sting into attributes. Then the function populates the Cat object attributes 
+	using the attributes from the string. Then the function returns the pointer to the Cat
+	object.*/
 	Cat* getCat(string* linePtr);
+	/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+	tokenise the sting into attributes. The function populates the Horse object attributes 
+	using the attributes from the string. Then the function returns the pointer to the Horse
+	object.*/
 	Horse* getHorse(string* linePtr);
+	
 	vector<string>* getAttributes(string* linePtr);
 };
 
+/*Definition of the transformToBase function.*/
 template<class Derived, class Base>
 vector<Base*> CSVFileReader::transformToBase(vector<Derived*> &derivedVec) {
   class vector<Base*> baseVec;
--- src/CSVFileReader.cpp
+++ src/CSVFileReader.cpp
@@ -3,8 +3,15 @@
 
 using namespace std;
 
+/*Constructor for the class which is used to read files to store the information into objects. 
+The objects are then saved in memory. The class provides methods to retrieve vectors of 
+pointers to these objects.*/
 CSVFileReader::CSVFileReader() {}
 
+/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+vector which stores pointers to Dog objects. The Dog objects have been created using attributes 
+which have been read from the file. The function returns the pointer to the vector of pointers to 
+Dog objects.*/
 vector<Dog*>* CSVFileReader::getDogs(ifstream* filePtr){
 	vector<Dog*>* dogVectsPtr = new vector<Dog*>();
 	if(filePtr->is_open()){
@@ -20,6 +27,10 @@ vector<Dog*>* CSVFileReader::getDogs(ifstream* filePtr){
 	return dogVectsPtr;
 }
 
+/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+vector which stores pointers to Cat objects. The Cat objects have been created using attributes 
+which have been read from the file. The function returns the pointer to the vector of pointers to 
+Cat objects.*/
 vector<Cat*>* CSVFileReader::getCats(ifstream* filePtr){
 	vector<Cat*>* catVectsPtr = new vector<Cat*>();
 	if(filePtr->is_open()){
@@ -35,6 +46,10 @@ vector<Cat*>* CSVFileReader::getCats(ifstream* filePtr){
 	return catVectsPtr;
 }
 
+/*Reads a file which is pointed to by a pointer. Then the function creates a pointer to the 
+vector which stores pointers to Horse objects. The Horse objects have been created using attributes 
+which have been read from the file. The function returns the pointer to the vector of pointers to 
+Horse objects.*/
 vector<Horse*>* CSVFileReader::getHorses(ifstream* filePtr){
 	vector<Horse*>* horseVectsPtr = new vector<Horse*>();
 	if(filePtr->is_open()){
@@ -50,6 +65,10 @@ vector<Horse*>* CSVFileReader::getHorses(ifstream* filePtr){
 	return horseVectsPtr;
 }
 
+/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+tokenise the sting into attributes. Then the function populates the Dog object attributes 
+using the attributes from the string. Then the function returns the pointer to the Dog
+object.*/
 Dog* CSVFileReader::getDog(string* linePtr){
 	vector<string>* attributesPtr = getAttributes(linePtr);
 	Dog *dogPtr = new Dog(attributesPtr->at(0),attributesPtr->at(1),attributesPtr->at(2),attributesPtr->at(6),attributesPtr->at(7),attributesPtr->at(3),attributesPtr->at(4),attributesPtr->at(5));
@@ -57,6 +76,10 @@ Dog* CSVFileReader::getDog(string* linePtr){
 	return dogPtr;
 }
 
+/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+tokenise the sting into attributes. Then the function populates the Cat object attributes 
+using the attributes from the string. Then the function returns the pointer to the Cat
+object.*/
 Cat* CSVFileReader::getCat(string* linePtr){
 	vector<string>* attributesPtr = getAttributes(linePtr);
 	Cat *catPtr = new Cat(attributesPtr->at(0),attributesPtr->at(1),attributesPtr->at(2),attributesPtr->at(6),attributesPtr->at(7),attributesPtr->at(3),attributesPtr->at(4),attributesPtr->at(5));
@@ -64,6 +87,10 @@ Cat* CSVFileReader::getCat(string* linePtr){
 	return catPtr;
 }
 
+/*Reads a string pointed to by a pointer. Calls getAttributes function on a string pointer to
+tokenise the sting into attributes. The function populates the Horse object attributes 
+using the attributes from the string. Then the function returns the pointer to the Horse
+object.*/
 Horse* CSVFileReader::getHorse(string* linePtr){
 	vector<string>* attributesPtr = getAttributes(linePtr);
 	Horse *horsePtr = new Horse(attributesPtr->at(0),attributesPtr->at(1),attributesPtr->at(2),attributesPtr->at(6),attributesPtr->at(7),attributesPtr->at(3),attributesPtr->at(4),attributesPtr->at(5));
@@ -71,6 +98,8 @@ Horse* CSVFileReader::getHorse(string* linePtr){
 	return horsePtr;
 }
 
+/*Reads a string pointed to by a pointer. The function splits the line by a comma delimeter 
+into sub-strings and returns the sub-strings.*/
 vector<string>* CSVFileReader::getAttributes(string* linePtr){
 	int commaIndex;
 	vector<string>* attributesPtr = new vector<string>();
