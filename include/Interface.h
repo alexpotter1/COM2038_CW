0b763521ac86fb0a703664cc463d9540b2a05db3
ob00218 <oliver.brahma.pearl@gmail.com>
2017-12-17 15:06:08 +0000

    Private string whitespace methods

--- include/Interface.h
+++ include/Interface.h
@@ -7,13 +7,13 @@ class Interface {
 public:
 	Interface(StorageManager& smanager);
 	void printAll();
-	string ltrim(string s, const char* t = " \t\n\r\f\v");
-	string rtrim(string s, const char* t = " \t\n\r\f\v");
-	string trim(string s, const char* t = " \t\n\r\f\v");
 	void getInput();
 private:
 	StorageManager storageManager;
 	string paternalTree[3];
 	int counter;
 	void getPaternalTree(string type, string name);
+	string ltrim(string s, const char* t = " \t\n\r\f\v");
+	string rtrim(string s, const char* t = " \t\n\r\f\v");
+	string trim(string s, const char* t = " \t\n\r\f\v");
 };
